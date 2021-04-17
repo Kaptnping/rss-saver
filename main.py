430b7d2076525c8f33bb9ea903141d9d6216d5e0
Kaptnping <41169292+Kaptnping@users.noreply.github.com>
2021-04-17 19:10:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,25 @@ feeds = db.feeds.find()
 count = 0
 
 for feed in feeds:
-response = feedparser.parse(feed["link"], etag=feed["etag"], modified=feed["modified"])
-etag = response.get('etag', '')
-modified = response.get('modified_parsed', '')
-db.feeds.update_one({"name": feed["name"]}, {"$set": {"etag": etag, "modified": modified}})
-for entry in response.entries:
-mydict = {}
-try:
-mydict["title"] = entry.title
-except:
-continue
-mydict["summary"] = BeautifulSoup(entry.get("summary", ''), "html.parser").get_text()
-mydict["date"] = entry.get("published", time.localtime(time.time()))
-mydict["link"] = entry.get("link", '')
-mydict["source"] = feed["name"]
-if db.entries.find_one({"title": mydict["title"]}):
-continue
-else:
-db.entries.insert_one(mydict)
-count += 1
+  response = feedparser.parse(feed["link"], etag=feed["etag"], modified=feed["modified"])
+  etag = response.get('etag', '')
+  modified = response.get('modified_parsed', '')
+  db.feeds.update_one({"name": feed["name"]}, {"$set": {"etag": etag, "modified": modified}})
+  for entry in response.entries:
+    mydict = {}
+    try:
+      mydict["title"] = entry.title
+    except:
+      continue
+    mydict["summary"] = BeautifulSoup(entry.get("summary", ''), "html.parser").get_text()
+    mydict["date"] = entry.get("published", time.localtime(time.time()))
+    mydict["link"] = entry.get("link", '')
+    mydict["source"] = feed["name"]
+    if db.entries.find_one({"title": mydict["title"]}):
+      continue
+    else:
+      db.entries.insert_one(mydict)
+      count += 1
 
 server = smtplib.SMTP_SSL("mail.cdtm.de", 465)
 server.login(os.getenv('CDTM_E_MAIL'), os.getenv('CDTM_PW'))
